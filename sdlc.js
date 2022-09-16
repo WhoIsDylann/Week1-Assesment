9e39374122bd49a85f62460280d64ffc132935b4
Dylan <dbc0728@gmail.com>
2022-09-16 09:59:22 -0400

    initial commit

---
+++ sdlc.js
@@ -0,0 +1,38 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Answer :
+
+----------------------------------
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+----------------------------------
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis_of_Requirements = "The second phase where teams consider the functional requirements of the project or solution" 
+const Design = "The third phase describes, in detail, the necessary specifications, features and operations that will satisfy the functional requirements of the proposed system which will be in place."
+const Implementation = "The fourth phase where majority of the code is written, and when the project is put into production."
+const Testing_and_integration = "The fifth phase involves systems integration and system testing of programs and procedures to determine if the proposed design meets the initial set of business goals."
+const Maintain = "The sixth and last phase is when end users can fine-tune the system, if they wish, to boost performance, add new capabilities or meet additional user requirements."
